e6419a150921a5f30545a1dc6f83510e8519f754
Naoki Takahashi <aho.mickey.mein@gmail.com>
2021-01-21 08:59:01 +0900

    公開･非公開のAPIを作った。フロントには全て返してしまっているので改修が必要

--- routes/website.js
+++ routes/website.js
@@ -17,7 +17,7 @@ router.get('/', (req, res) => {
 	let param;
 	pool.getConnection()
 		.then(connection => {
-			connection.query('SELECT MAX(created_at) as created_at, user_id, room_id, user_name, room_name, address FROM transactions NATURAL JOIN users NATURAL JOIN rooms GROUP BY user_id')
+			connection.query('SELECT MAX(created_at) as created_at, user_id, room_id, user_name, room_name, address, is_public FROM transactions NATURAL JOIN users NATURAL JOIN rooms GROUP BY user_id')
 				.then((rows) => {
 					param = JSON.stringify(rows);
 					res.header('Content-Type', 'application/json; charset=utf-8');
@@ -26,5 +26,37 @@ router.get('/', (req, res) => {
 				});
 		});
 });
+
+router.get('/:id', (req, res) => {
+	let param;
+	let userId = req.params.id;
+	pool.getConnection()
+		.then(connection => {
+			connection.query('SELECT * FROM users WHERE user_id = ?', [userId])
+				.then((rows) => {
+					param = JSON.stringify(rows);
+					res.header('Content-Type', 'application/json; charset=utf-8');
+					res.send(param);
+					connection.end();
+				});
+		});
+})
+
+router.put('/:id/:is_public', (req, res) => {
+	let param;
+	let userId = req.params.id;
+	let is_public = req.params.is_public
+
+	pool.getConnection()
+	.then(connection => {
+		connection.query('UPDATE users SET is_public = ? WHERE user_id = ?', [is_public, userId])
+			.then((rows) => {
+				param = JSON.stringify(rows);
+				res.header('Content-Type', 'application/json; charset=utf-8');
+				res.send(param);
+				connection.end();
+			});
+	});
+})
 
 module.exports = router;
